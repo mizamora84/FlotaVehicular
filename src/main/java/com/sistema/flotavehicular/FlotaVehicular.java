f2e54d8e7bc82f78294b9af04a8e012af8d5e3f1
CristianOsorio1006048507 <150768876+CristianOsorio1006048507@users.noreply.github.com>
2023-11-18 00:23:43 -0500

    Update FlotaVehicular.java

--- src/main/java/com/sistema/flotavehicular/FlotaVehicular.java
+++ src/main/java/com/sistema/flotavehicular/FlotaVehicular.java
@@ -1,5 +1,6 @@
 package com.sistema.flotavehicular;
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class FlotaVehicular {
 
@@ -9,33 +10,88 @@ public class FlotaVehicular {
         Vehiculo[] flota = new Vehiculo[3];
         Conductor[] conductores = new Conductor[1];
         Dispositivo[] dispositivos = new Dispositivo[1];
+        
+        
+        boolean continuar = true;
+        int vehiculoIndex = 0; // Contador para los vehículos registrados
+        
+    while (continuar) {
+        String menuPrincipal = "1. Administrador\n" +
+                           "2. Operador\n" +
+                           "3. Sistema\n" +
+                           "4. Salir\n" +
+                           "Seleccione una opción: ";
+    String opcionStr = JOptionPane.showInputDialog(menuPrincipal);
+    int opcion = opcionStr != null ? Integer.parseInt(opcionStr) : 4;
+    
+    switch (opcion) {
+        case 1:            
+            boolean adminContinuar = true;
+            while (adminContinuar) {
+                String menuAdmin = "1. Agregar Vehículos\n" +
+                                   "2. Agregar Tareas\n" +
+                                   "3. Volver\n" +
+                                   "Seleccione una opción: ";
+                String opcionAdminStr = JOptionPane.showInputDialog(menuAdmin);
+                int opcionAdmin = opcionAdminStr != null ? Integer.parseInt(opcionAdminStr) : 3;
 
-        // Utilizar la fábrica para crear un vehículo pesado y agregarlo al arreglo
-        VehiculoFactory pesadoFactory = new PesadoFactory();
-        flota[0] = pesadoFactory.crearVehiculo("Volvo", "ABC123", 5000);
+                switch (opcionAdmin) {
+                    case 1:
+                        if (vehiculoIndex >= flota.length) {
+                            JOptionPane.showMessageDialog(null, "Capacidad máxima de vehículos alcanzada.");
+                        break;
+                }
+                // Lógica para agregar vehículos
+                String tipoVehiculo = JOptionPane.showInputDialog("Ingrese tipo de vehículo (Pesado/Liviano):");
+                String marca = JOptionPane.showInputDialog("Ingrese marca del vehículo:");
+                String matricula = JOptionPane.showInputDialog("Ingrese matrícula del vehículo:");
+                int capacidad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese capacidad del vehículo:"));
 
-        // Crear un conductor y agregarlo al arreglo
-        Conductor conductor = new Conductor("Juan", 22);
-        conductores[0] = conductor;
+                VehiculoFactory vehiculoFactory = tipoVehiculo.equalsIgnoreCase("Pesado") ? new PesadoFactory() : new LivianoFactory();
+                flota[vehiculoIndex] = vehiculoFactory.crearVehiculo(marca, matricula, capacidad);
 
-        // Utilizar la fábrica para crear un vehículo liviano y agregarlo al arreglo
-        VehiculoFactory livianoFactory = new LivianoFactory();
-        flota[1] = livianoFactory.crearVehiculo("Toyota", "XYZ789", 5);
-        flota[2] = livianoFactory.crearVehiculo("Toyota", "XYZ789", 5);
+                String nombreConductor = JOptionPane.showInputDialog("Ingrese nombre del conductor:");
+                int edadConductor = Integer.parseInt(JOptionPane.showInputDialog("Ingrese edad del conductor:"));
+                conductores[vehiculoIndex] = new Conductor(nombreConductor, edadConductor);
 
-        // Utilizar la fábrica para crear un dispositivo y agregarlo al arreglo
-        DispositivoFactory sensorFactory = new SensorFactory();
-        dispositivos[0] = sensorFactory.crearDispositivo("gps", 22);
-        JOptionPane.showMessageDialog(null,"       ¿Por qué sos tan hermoso, Cristian?      ");
-
-        // Iterar sobre el arreglo y mostrar el tipo de cada vehículo
-        for (Vehiculo vehiculo : flota) {
-            vehiculo.mostrarTipo();
-        }
-
-        // Iterar sobre el arreglo y mostrar información de cada dispositivo
-        for (Dispositivo dispositivo : dispositivos) {
-            dispositivo.mostrarInfoDispositivo();
+                vehiculoIndex++;
+                JOptionPane.showMessageDialog(null, "Vehículo y conductor registrados.");
+                break;
+            case 2:
+                // Lógica para agregar tareas
+                // Aquí puedes agregar el código para manejar tareas
+                break;
+            case 3:
+                adminContinuar = false;
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opción no válida.");
+                break;
+                }
+            }
+            break;
+        case 2:
+            // Lógica para el operador
+            StringBuilder reporte = new StringBuilder("Reporte de Vehículos:\n");
+            for (int i = 0; i < vehiculoIndex; i++) {
+                if (flota[i] != null) {
+                    flota[i].mostrarTipo();
+                }
+            }
+            JOptionPane.showMessageDialog(null, reporte.toString());
+            break;
+        case 3:
+            // Lógica para el sistema
+            JOptionPane.showMessageDialog(null, "Información del sistema:\n[Detalles del sistema]");
+            break;
+        case 4:
+            continuar = false;
+            JOptionPane.showMessageDialog(null, "Saliendo del programa.");
+            break;
+        default:
+            JOptionPane.showMessageDialog(null, "Opción no válida.");
+            break;
+            }
         }
     }
-}
+} 
